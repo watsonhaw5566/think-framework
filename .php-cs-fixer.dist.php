14da1a78d60afd69fc6ffeb9f76ce8a0700f77c8
yunwuxin <448901948@qq.com>
2026-01-09 18:53:13 +0800

    style(format): 优化 PHP CS Fixer 配置规则

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -13,6 +13,23 @@ return (new Config())
         'binary_operator_spaces' => [
             'default' => 'align_single_space_minimal',
         ],
+        'blank_line_before_statement' => [
+            'statements' => [
+                'continue',
+                'declare',
+                'return',
+                'throw',
+                'try',
+            ],
+        ],
+        'concat_space' => [
+            'spacing' => 'one',
+        ],
+        'global_namespace_import' => [
+            'import_classes'   => true,
+            'import_constants' => false,
+            'import_functions' => false,
+        ],
     ])
     ->setFinder($finder)
 ;
